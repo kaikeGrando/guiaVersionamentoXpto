c22c5eaa758cea757309d1ac6f88a0336e664959
SeuUsuario <SeuEmail>
2026-08-29 11:25:58 -0300

    fix test_sum

--- test_sum.js
+++ test_sum.js
@@ -1,6 +1,6 @@
+const {sum} = require("./app.js")
 
-const appFunctions = require("./app.js")
 console.log(appFunctions)
-if(appFunctions.soma(5,7)!=12){
+if(sum(5,7)!=12){
 throw console.error("ERRO NA SOMA, RESULTADO ESPERADO: 12, OBTIDO: "+ soma(5,7));
 }
